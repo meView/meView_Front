e0c4940f9963d86562189970368b80d7c5c79585
taegeon-kim-21 <aksen5240@gmail.com>
2024-02-18 19:23:10 +0900

    Add global background color

--- src/layout/App.js
+++ src/layout/App.js
@@ -1,12 +1,19 @@
 import React from "react";
 import { Mobile, PC } from "./MediaQuery";
-import styled from "styled-components";
+import styled, { createGlobalStyle } from "styled-components";
 import BodyContent from "./BodyContent";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background-color: #222222;
+  }
+`;
 
 const PcView = styled.div`
   height: 100vh;
   width: 500px;
   margin: 0 auto;
+  background-color: var(--Gray-15);
 `;
 
 const MobileView = styled.div`
@@ -16,6 +23,7 @@ const MobileView = styled.div`
 function App() {
   return (
     <>
+      <GlobalStyle />
       <PC>
         <PcView>
           <BodyContent />
