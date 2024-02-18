b4f074f8dba193f067509b5bd654f36d4b3a42d6
yiseungyun <seungxxyun@gmail.com>
2024-02-18 14:17:52 +0900

    Fix testcode page file for question page test

--- src/Main.js
+++ src/Main.js
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import About from "./pages/About";
-import TestCode from "./pages/TestCode";
+import Question from "./pages/Question";
 import TestCode2 from "./pages/TestCode2";
 
 function Main() {
@@ -10,7 +10,7 @@ function Main() {
       <Routes>
         <Route path="/" element={<App />}>
           <Route path="about" element={<About />} />
-          <Route path="testcode" element={<TestCode />} />
+          <Route path="testcode" element={<Question />} />
           <Route path="testcode2" element={<TestCode2 />} />
         </Route>
       </Routes>
