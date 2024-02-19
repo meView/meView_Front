b3b51222b82857401fcf9c3e96e45455d855041e
taegeon-kim-21 <aksen5240@gmail.com>
2024-02-20 01:50:52 +0900

    Add MainBottom

--- src/components/home/Bottombar.js
+++ src/components/home/Bottombar.js
@@ -1,10 +1,35 @@
 import styled from "styled-components";
 
+const Container = styled.div`
+  position: fixed; /* 화면에 고정 */
+  bottom: 0; /* 화면 바닥에 정렬 */
+  max-width: 500px; /* 최대 너비 설정 */
+  width: 100%; /* 화면 너비와 같게 설정 */
+`;
+const Content = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  
+
+  .rightside {
+    background: none;
+    border: none;
+    cursor: pointer;
+    padding: 0;
+  }
+`;
+
 function Bottombar() {
   return (
-    <div>
-      <h1>Bottombar</h1>
-    </div>
+    <Container>
+      <Content>
+        <div className="leftside"></div>
+        <button className="rightside">
+          <img alt="createQ" src="/image/create-q-button.svg" />
+        </button>
+      </Content>
+    </Container>
   );
 }
 
