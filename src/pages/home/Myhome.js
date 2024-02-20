f574362ee2fdec7e79dd4e4048ac318fd8fe1113
yiseungyun <seungxxyun@gmail.com>
2024-02-21 03:00:07 +0900

    Add review list for home page

--- src/pages/home/Myhome.js
+++ src/pages/home/Myhome.js
@@ -1,16 +1,30 @@
 import Bottombar from "../../components/home/Bottombar";
 import TopNav from "../../components/home/TopNav";
 import styled from "styled-components";
+import NoReview from "../../components/home/NoReview";
+import ReviewList from "../../components/home/ReviewList";
+import { useRecoilValue } from "recoil";
+import { questionFormState } from "../../recoil/HomeAtom";
 
 const Container = styled.div`
-  
+  height: 100vh;
+  position: relative;
+  background-color: var(--Gray-15);
 `;
 
 function Myhome() {
+
+  /* 전역 상태에 저장해둔 생성 질문지 가져오기 */
+  const reviewList = useRecoilValue(questionFormState);
+
   return (
     <Container>
       <TopNav />
-      <h1>body</h1>
+      {
+        reviewList
+        ? <ReviewList/>  
+        : <NoReview/>        
+      }
       <Bottombar />
     </Container>
   );
