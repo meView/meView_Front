73b9102a922a083506458522dffae120c2188a2c
taegeon-kim-21 <aksen5240@gmail.com>
2024-02-25 17:34:50 +0900

    Add ViewNav component to MeviewStrength page

---
+++ src/components/meview/ViewNav.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+
+`;
+
+function ViewNav() {
+  <Container>
+    <h1> ViewNav</h1>
+  </Container>
+}
+
+export default ViewNav;
