b415d7e94ac81cbd44dfb3df0d1ad335b9383277
taegeon-kim-21 <aksen5240@gmail.com>
2024-02-26 03:13:21 +0900

    ProjectView working

---
+++ src/components/meview/project/ProjectList.js
@@ -0,0 +1,53 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  background-color: var(--Gray-14);
+  margin: 0px 20px;
+  padding: 12px;
+  color: white;
+  border-radius: 8px;
+  display: flex;
+  border-right: solid 8px var(--Gray-13);
+  position: relative;
+`;
+const List = styled.div`
+  display: flex;
+  align-items: center;
+
+  .pjname {
+    margin-left: 12px;
+    font-size: 18px;
+    font-weight: bold;
+  }
+  .countnum {
+    position: absolute;
+    right: 16px;
+    border-radius: 8px;
+    width: 32px;
+    height: 32px;
+    color: var(--primary);
+    background-color: var(--Gray-13);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 14px;
+    font-weight: bold;
+  }
+`;
+
+
+function ProjectList(props) {
+  return (
+    <Container>
+      <List>
+        <img src="/image/reviewfoldericon.svg" alt="review folder icon" />
+        {/* <span className="pjname">프로젝트명</span>
+        <div className="countnum">+4</div> */}
+        <span className="pjname">{props.projectname}</span>
+        <div className="countnum">+{props.count}</div>
+      </List>
+    </Container>
+  );
+}
+
+export default ProjectList;
