75d7aaefa40391bb6876070ba487fd49569a0f4b
taegeon-kim-21 <aksen5240@gmail.com>
2024-02-25 17:16:36 +0900

    Delete Footer

--- src/layout/BodyContent.js
+++ src/layout/BodyContent.js
@@ -1,16 +1,12 @@
-import Footer from "./Footer";
+
 import { Outlet } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 
 function BodyContent() {
-  const location = useLocation()
-  const showFooter = 
-  location.pathname !== '/testcode' && location.pathname !== '/home' && location.pathname !== '/answer';
-  
+
   return (
     <div>
       <Outlet />
-      {showFooter && <Footer />}
     </div>
   );
 }
