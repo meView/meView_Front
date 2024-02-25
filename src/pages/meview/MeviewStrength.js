5b30fac1e072441498b60f49485609a28e0c0bb4
taegeon-kim-21 <aksen5240@gmail.com>
2024-02-25 17:31:30 +0900

    Add TopNav component and update MeviewStrength page

--- src/pages/meview/MeviewStrength.js
+++ src/pages/meview/MeviewStrength.js
@@ -1,10 +1,16 @@
+import styled from "styled-components";
+import TopNav from "../../components/meview/TopNav";
 
+const Container = styled.div`
+  height: 100vh;
+`;
 
 function MeviewStrength() {
   return (
-    <div>
+    <Container>
+      <TopNav />
       <h1>MeviewStrength</h1>
-    </div>
+    </Container>
   );
 }
 
