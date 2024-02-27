a633bb6487a8e50b7b578ba81c4ca47111002b8c
taegeon-kim-21 <aksen5240@gmail.com>
2024-02-27 12:35:09 +0900

    Refactor projectlist.json and ProjectCollection.js

--- src/components/meview/project/ProjectCollection.js
+++ src/components/meview/project/ProjectCollection.js
@@ -2,6 +2,8 @@ import styled from "styled-components";
 import ProjectList from "./ProjectList";
 import { useRecoilValue } from "recoil";
 import { projectListState } from "../../../recoil/ProjectListAtom";
+import NoReview from "components/home/NoReview";
+import Bottombar from "components/home/Bottombar";
 
 const Container = styled.div`
   margin: 16px 0 0;
@@ -12,8 +14,18 @@ const PJList = styled.div`
   padding: 16px 0 0;
 `;
 
+
 function ProjectCollection() {
   const projectData = useRecoilValue(projectListState);
+
+  if (!projectData || projectData.length === 0) {
+    return (
+      <>
+        <NoReview />
+        <Bottombar />
+      </>
+    );
+  }
 
   return (
     <Container>
