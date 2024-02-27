87e912e047ecd79fae0bd4a3fef990e135351775
taegeon-kim-21 <aksen5240@gmail.com>
2024-02-27 15:34:08 +0900

    Answer Page Update

--- src/components/answer/WeaknessAnswerBox.js
+++ src/components/answer/WeaknessAnswerBox.js
@@ -1,13 +1,13 @@
-import { useEffect, useState } from 'react';
-import { useRecoilState } from 'recoil';
-import styled from 'styled-components'
-import { answerState } from '../../recoil/AnswerAtom';
+import { useEffect, useState } from "react";
+import { useRecoilState } from "recoil";
+import styled from "styled-components";
+import { answerState } from "../../recoil/AnswerAtom";
 
 const Container = styled.div`
   width: 100%;
   margin-bottom: 48px;
   background-color: var(--Gray-15);
-  
+
   .warning {
     margin-top: 20px;
     margin-left: 20px;
@@ -24,7 +24,7 @@ const Container = styled.div`
     right: 36px;
     font-size: var(--button-03);
     font-weight: var(--font-weight-bold);
-    color: #6F6F6F;
+    color: #6f6f6f;
   }
   .title-box {
     margin: 0 20px 16px 20px;
@@ -40,10 +40,10 @@ const Container = styled.div`
     color: var(--Gray-09);
     line-height: 26px;
   }
-`
+`;
 const TextArea = styled.textarea`
   &::-webkit-scrollbar {
-    width: 7px; 
+    width: 7px;
   }
   &::-webkit-scrollbar-thumb {
     background: var(--Gray-12);
@@ -76,8 +76,8 @@ const TextArea = styled.textarea`
   `}
   &:focus {
     ${({ $textState }) =>
-    $textState === "writing" &&
-    `
+      $textState === "writing" &&
+      `
     outline: 1px solid var(--primary);
   `}
   }
@@ -93,14 +93,15 @@ const TextLength = styled.span`
 `;
 
 function WeaknessAnswerBox(props) {
-
   /* 글자 수 세기 + 입력 값 저장 */
   const [answer, setAnswer] = useRecoilState(answerState);
-  const [inputText, setInputText] = useState(answer.weaknessReview[props.chip]|| "");
-  
-  useEffect(()=>{
+  const [inputText, setInputText] = useState(
+    answer.weaknessReview[props.chip] || ""
+  );
+
+  useEffect(() => {
     setInputText(answer.weaknessReview[props.chip]);
-  }, [props.chip])
+  }, [props.chip]);
 
   const handleChange = (e) => {
     if (e.target.value.length <= 500) {
@@ -109,7 +110,7 @@ function WeaknessAnswerBox(props) {
         ...answer,
         weaknessReview: {
           ...answer.weaknessReview,
-          [props.chip]: e.target.value
+          [props.chip]: e.target.value,
         },
       });
     }
@@ -123,7 +124,7 @@ function WeaknessAnswerBox(props) {
   const handleBlur = () => {
     setIsFocused(false);
   };
-  
+
   const getTextState = (length) => {
     if (length >= 500) return "error";
     if (length > 0 && length < 500) return "writing";
@@ -134,46 +135,63 @@ function WeaknessAnswerBox(props) {
 
   /* 선택 칩 */
   const chip_dictionary = {
-    'communication': '소통능력', 
-    'observation': '관찰력',
-    'judgement': '판단력',
-    'execution': '실행력',
-    'friendliness': '친화력',
-    'listening': '경청능력',
-    'perseverance': '끈기력',
-  }
+    communication: "소통능력",
+    observation: "관찰력",
+    judgement: "판단력",
+    execution: "실행력",
+    friendliness: "친화력",
+    listening: "경청능력",
+    perseverance: "끈기력",
+  };
+  // 멘트 칩
+  const chip_ment_dictionary = {
+    communication:
+      "ex) 말하려는 바가 명료하지 않아서 이해가 어려울 때가 있어요. 생각을 한번 정리한 후에 이야기해주면 좋겠어요.",
+    observation:
+      "ex) 큰 그림은 잘 그리지만 디테일을 놓칠 때가 있어요. 사소한 부분도 발견해 내는 관찰력을 기른다면 더 나은 인재로 성장할 거예요. ",
+    judgement:
+      "ex) 완벽을 추구하다 보니 중요한 순간에 결정을 못 할 때가 있어 아쉬웠어요. 신속하게 판단하는 능력을 기른다면 더 나은 인재로 성장할 거예요. ",
+    execution:
+      "ex) 실제로 행동하지 않아서 방치되는 아이디어가 많아 아쉬워요. 실행력을 기른다면 더 나은 인재로 성장할 거예요. ",
+    friendliness:
+      "ex) 문제가 발생해도 팀원에게 공유하지 않는 모습이 조금 아쉬웠어요. 협조적인 태도를 기른다면 팀원들과 시너지를 낼 수 있을 거예요.",
+    listening:
+      "ex) 주장이 뚜렷해서 가끔 다른 의견을 듣지 않을 때가 있어요. 상대방의 입장을 공감하는 자세로 경청해주면 좋겠어요.",
+    perseverance:
+      "ex) 실행력에 비해 뒷심은 조금 아쉬워요. 어려운 과제에 직면해도 끝까지 마무리하는 끈기력까지 갖춘다면 실행력과 함께 시너지를 낼 수 있을 거예요. ",
+  };
 
   return (
     <Container>
-      <div className='title-box'>
-        <p className='title'>{chip_dictionary[props.chip]}을 고른 이유가 궁금해요</p>
-        <p className='desc'>구체적인 사례를 적어주시면 더 좋아요</p>
+      <div className="title-box">
+        <p className="title">
+          {chip_dictionary[props.chip]}을 고른 이유가 궁금해요
+        </p>
+        <p className="desc">구체적인 사례를 적어주시면 더 좋아요</p>
       </div>
       <div className="answer">
         <TextArea
           name="review"
           maxLength="500"
           value={inputText}
-          placeholder="민지님과 OO 프로젝트를 진행할 때 oo아이디어에 관해 팀원의 의견을 수렴하는 과정에서 이야기를 경청하는 모습이 인상 깊었어요 :)"
+          placeholder={chip_ment_dictionary[props.chip]}
           onChange={handleChange}
           $textState={textState}
           onFocus={handleFocus}
           onBlur={handleBlur}
         />
         <div className="count">
-          <TextLength $textState={textState} $isFocused={isFocused}>{
-            inputText?.length || 0
-          }/</TextLength>
+          <TextLength $textState={textState} $isFocused={isFocused}>
+            {inputText?.length || 0}/
+          </TextLength>
           500자
         </div>
       </div>
-      {
-        textState === "error" 
-        ? <div className="warning">
-            <img alt="warning message" src="./image/warning-500msg.svg"/>
-          </div>
-        : null
-      }
+      {textState === "error" ? (
+        <div className="warning">
+          <img alt="warning message" src="./image/warning-500msg.svg" />
+        </div>
+      ) : null}
     </Container>
   );
 }
--- src/pages/answer/Start.js
+++ src/pages/answer/Start.js
@@ -1,20 +1,39 @@
-import { useRecoilState } from 'recoil'
-import styled from 'styled-components'
-import { pageState } from '../../recoil/AnswerAtom'
+import { useRecoilState } from "recoil";
+import styled from "styled-components";
+import { pageState } from "../../recoil/AnswerAtom";
 
 const Container = styled.div`
   width: 100%;
   max-width: 500px;
   height: 100vh;
-  background-image: url('./image/answer-background.svg');
+  background-image: url("./image/answer-background.svg");
   background-repeat: no-repeat;
   background-size: cover;
   background-position: center;
-`
+  color: var(--Gray-15);
+  font-weight: bold;
+`;
 const Top = styled.div`
-`
-const Body = styled.div`
-`
+  padding: 56px 20px;
+  .highlight {
+    font-size: 36px;
+    display: inline;
+    box-shadow: inset 0 -20px 0 rgba(23, 23, 23, 0.16);
+  }
+  .text {
+    font-size: 24px;
+  }
+  .text2 {
+    font-size: 18px;
+    margin-top: 4px;
+  }
+  .subtitle {
+    display: flex;
+    margin-top: 12px;
+    color: rgba(23, 23, 23, 0.6);
+  }
+`;
+
 const Bottom = styled.div`
   position: fixed;
   bottom: 24px;
@@ -30,26 +49,43 @@ const Bottom = styled.div`
   .button {
     width: 100%;
   }
-`
+`;
 
 function Start() {
   const [page, setPage] = useRecoilState(pageState);
   return (
     <Container>
       <Top>
+        <span className="highlight">닉네임누구님</span>
+        <span className="text">
+          이<br></br>
+        </span>
+        <span className="highlight">프로젝트어디</span>
+        <span className="text">
+          에서<br></br>
+        </span>
+        <span className="text2">
+          본인의 강점, 약점 or 강점+약점에 대한 리뷰를 원해요
+        </span>
+        <span className="subtitle">모든 답변은 익명으로 전달돼요</span>
       </Top>
-      <Body>
 
-      </Body>
       <Bottom>
-        <button className='button-img' onClick={()=>{
-          setPage(page+1);
-        }}>
-          <img className='button' alt="review start button" src="./image/review-start-button.svg"/>
+        <button
+          className="button-img"
+          onClick={() => {
+            setPage(page + 1);
+          }}
+        >
+          <img
+            className="button"
+            alt="review start button"
+            src="./image/review-start-button.svg"
+          />
         </button>
       </Bottom>
     </Container>
-  )
+  );
 }
 
 export default Start;
