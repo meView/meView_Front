093bf0cbba358e96afba861863ff6737e93fe6a5
yiseungyun <seungxxyun@gmail.com>
2024-03-05 17:41:20 +0900

    Fix text align to no review page

--- src/components/home/NoReview.js
+++ src/components/home/NoReview.js
@@ -3,13 +3,18 @@ import styled from "styled-components";
 const Container = styled.div`
   font-weight: var(--font-weight-bold);
   text-align: center;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  padding-bottom: 130px;
-  transform: translate(-50%, -50%);
+  width: 100%;
+  height: calc(100vh - 208px);
   color: var(--Gray-02);
+  position: relative;
 
+  .text-box {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    white-space: nowrap;
+  }
   .title {
     font-size: var(--subtitle-01);
     line-height: 28px;
@@ -24,8 +29,10 @@ const Container = styled.div`
 function NoReview() {
   return (
     <Container>
-      <p className="title">아직 리뷰가 없어요!</p>
-      <p className="description">나의 강약점을 발굴하러 가요!</p>
+      <div className="text-box">
+        <p className="title">아직 리뷰가 없어요!</p>
+        <p className="description">나의 강약점을 발굴하러 가요!</p>
+      </div>
     </Container>
   )
 }
