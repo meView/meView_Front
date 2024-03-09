6b5c2ed5d4185771a9a1f5158184eb0cc8fb09bd
yiseungyun <seungxxyun@gmail.com>
2024-03-09 11:27:30 +0900

    Fix question error in creating duplicate questionnaires

--- src/api/Question_API.js
+++ src/api/Question_API.js
@@ -2,10 +2,12 @@ import axios from "axios";
 
 const url_postQuestion = `${process.env.REACT_APP_SERVER_ADDRESS}/question/create`;
 export const postQuestion = async (data, access_token) => {
-  const response = await axios.post(url_postQuestion, data, {
-		headers : {
-			'Authorization' : `Bearer ${access_token}`
-		}
-  });
-  return response.data;
+  if (data.answer1 !== "" && data.answer2 !== "" && data.answer3 !== "") {
+    const response = await axios.post(url_postQuestion, data, {
+      headers : {
+        'Authorization' : `Bearer ${access_token}`
+      }
+    });
+    return response.data;
+  }
 };
